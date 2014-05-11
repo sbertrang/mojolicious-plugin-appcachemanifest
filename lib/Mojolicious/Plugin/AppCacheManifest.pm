54778b47a042d5dcc329f6438a07064769e718b6
Simon Bertrang <janus@errornet.de>
2014-05-11 23:35:29 +0200

    implement caching

--- lib/Mojolicious/Plugin/AppCacheManifest.pm
+++ lib/Mojolicious/Plugin/AppCacheManifest.pm
@@ -3,6 +3,7 @@ package Mojolicious::Plugin::AppCacheManifest;
 use Mojo::Base qw( Mojolicious::Plugin );
 
 our $VERSION = "0.01";
+our %MANIFEST;
 
 sub register
 {
@@ -18,8 +19,6 @@ sub register
 	my $retype = qr!\A text / cache \- manifest \b!x;
 	my $dirs = $app->static->paths();
 
-	$self->{timeout} = $timeout;
-
 	$app->log->info( "setting up " . __PACKAGE__ . " $VERSION: @$dirs" );
 	$app->hook( after_dispatch => sub {
 		my $tx = shift->tx();
@@ -32,12 +31,30 @@ sub register
 		return unless # mime type matches as well
 		    ( $res->headers->content_type() // "" ) =~ $retype;
 
-		# extract manifest information
-		my $manifest = $self->parse( $res->body() );
-		my $last_modified = $self->max_last_modified( $manifest, $res->content->asset->path(), $dirs );
+		my $path = $res->content->asset->path();
+		my $mtime = ( stat( $path ) )[9];
+		my $time = time();
+		my $last_modified;
+		my $output;
 
-		$res->body( $self->generate( $manifest, $last_modified ) );
+		if ( exists( $MANIFEST{ $path } ) &&
+		             $MANIFEST{ $path }[0] >= $mtime &&
+		             $MANIFEST{ $path }[1] + $timeout > $time ) {
+			$last_modified = $MANIFEST{ $path }[2];
+			$output = $MANIFEST{ $path }[3];
+		}
+		else {
+			my $manifest = $self->parse( $res->body() );
+
+			$last_modified = $self->max_last_modified( $manifest, $path, $dirs, $mtime );
+			$output = $self->generate( $manifest, $last_modified );
+
+			$MANIFEST{ $path } = [ $last_modified->epoch(), $time, $last_modified, $output ]
+				if $timeout > 0;
+		}
+
 		$res->headers->last_modified( $last_modified );
+		$res->body( $output );
 	} );
 }
 
@@ -75,8 +92,7 @@ sub parse
 
 sub max_last_modified
 {
-	my ( $self, $manifest, $path, $dirs ) = @_;
-	my $maxmtime = ( stat( $path ) )[9];
+	my ( $self, $manifest, $path, $dirs, $maxmtime ) = @_;
 	my @paths = map Mojo::URL->new( $_ )->path(), @{ $manifest->{CACHE} };
 
 	for my $parts ( grep $_->[0] ne "..", map $_->canonicalize->parts(), @paths ) {
