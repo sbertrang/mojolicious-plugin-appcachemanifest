6f8222634123163fafcd0ac2ad106b338b6833e2
Simon Bertrang <janus@errornet.de>
2014-05-19 21:16:17 +0200

    needs at least 3.12

--- lib/Mojolicious/Plugin/AppCacheManifest.pm
+++ lib/Mojolicious/Plugin/AppCacheManifest.pm
@@ -2,7 +2,7 @@ package Mojolicious::Plugin::AppCacheManifest;
 
 use Mojo::Base qw( Mojolicious::Plugin );
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 our %HEADERS = map +( $_, undef ), qw( CACHE FALLBACK NETWORK SETTINGS );
 our %CACHE;
 
